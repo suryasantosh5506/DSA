8b9ecbfe6f8a5f045053e4cf82fb922e4c109ab0
Santosh Reddy <santoshreddy.ksb@gmail.com>
2026-03-25 07:48:27 +0530

    Implemented TreeNode definition

--- DSA/binary trees/94. Binary Tree Inorder Traversal(Morris threaded Implementation).cpp
+++ DSA/binary trees/94. Binary Tree Inorder Traversal(Morris threaded Implementation).cpp
@@ -3,17 +3,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+class TreeNode{
+public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int val){
+        this->val=val;
+        this->left=nullptr;
+        this->right=nullptr;
+    }
+};
+
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
