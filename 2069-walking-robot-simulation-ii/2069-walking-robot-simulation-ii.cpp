2a1d7fe3479fd39b3f2cbd4cb67a22266c3a1e65
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-04-07 07:45:11 +0530

    Time: 303 ms (5.17%), Space: 127.3 MB (100%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
@@ -0,0 +1,69 @@
+class Robot {
+public:
+
+    int n,m;
+    int x=0,y=0;
+    string direction="East";
+
+    Robot(int width, int height) {
+        n=width;
+        m=height;
+    }
+    
+    void step(int num) {
+        int cycle = 2 * (n + m - 2);
+        int steps = num % cycle;
+
+        if (steps == 0 && num > 0) {
+            steps = cycle;
+        }
+
+        for(int i=0;i<steps;i++){
+            if(direction=="East"){
+                if(x+1>=n){
+                    direction="North";
+                    y++;
+                }else {
+                    x++;
+                }
+            }else if(direction=="West"){
+                if(x-1<0){
+                    direction="South";
+                    y--;
+                }else{
+                    x--;
+                }
+            }else if(direction=="North"){
+                if(y+1>=m){
+                    direction="West";
+                    x--;
+                }else{
+                    y++;
+                }
+            }else if(direction=="South"){
+                if(y-1<0){
+                    direction="East";
+                    x++;
+                }else{
+                    y--;
+                }
+            }
+        }
+    }
+    
+    vector<int> getPos() {
+        return {x,y};
+    }
+    
+    string getDir() {
+        return direction;
+    }
+};
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot* obj = new Robot(width, height);
+ * obj->step(num);
+ * vector<int> param_2 = obj->getPos();
+ * string param_3 = obj->getDir();
+ */
