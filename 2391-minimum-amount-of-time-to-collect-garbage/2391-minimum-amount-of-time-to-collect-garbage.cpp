0d6bc244f79592b5af5d401bd84c5e02a42ac665
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-04-10 08:13:18 +0530

    Time: 12 ms (73.7%), Space: 107 MB (96.16%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int papertime=0;
+        int garbagetime=0;
+        int metaltime=0;
+
+        int lastg=0,lastp=0,lastm=0;
+
+        int n = garbage.size();
+
+        for(int i=0;i<n;i++){
+            for(auto x:garbage[i]){
+                if(x=='P'){
+                    papertime++;
+                    lastp=i;
+                }
+                if(x=='M'){
+                    metaltime++;
+                    lastm=i;
+                }
+                if(x=='G'){
+                    garbagetime++;
+                    lastg=i;
+                }
+            }
+        }
+
+        for (int i = 0; i < lastp; i++) papertime += travel[i];
+        for (int i = 0; i < lastg; i++) garbagetime += travel[i];
+        for (int i = 0; i < lastm; i++) metaltime += travel[i];
+
+        return papertime+garbagetime+metaltime;
+    }
+};
