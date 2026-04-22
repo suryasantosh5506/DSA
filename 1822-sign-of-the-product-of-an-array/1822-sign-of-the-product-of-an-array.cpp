3615f814751cc9e7d3d514f2de61f03a9521fe07
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-04-23 01:26:43 +0530

    Time: 0 ms (100%), Space: 13.8 MB (49.58%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int neg=0;
+        for(auto x:nums){
+            if(x<0) neg++;
+            if(x==0) return 0;;
+        }
+        return (neg%2==0)?1:-1;
+    }
+};
