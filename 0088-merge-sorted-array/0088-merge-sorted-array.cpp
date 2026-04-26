0a4451b3a7c1bcabd79aee238560d46d500533ce
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-04-26 16:22:58 +0530

    Time: 0 ms (100%), Space: 12.5 MB (8.68%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -6,19 +6,12 @@ public:
         int index=0;
 
         while(i<m && j<n){
-            if(arr[i]<nums2[j]){
-                nums1[index++]=arr[i++];
-            }else{
-                nums1[index++]=nums2[j++];
-            }
+            if(arr[i]<nums2[j]) nums1[index]=arr[i++];
+            else nums1[index]=nums2[j++];
+            index++;
         }
 
-        while(i<m){
-            nums1[index++]=arr[i++];
-        }
-
-        while(j<n){
-            nums1[index++]=nums2[j++];
-        }
+        while(i<m) nums1[index++]=arr[i++];
+        while(j<n) nums1[index++]=nums2[j++];
     }
 };
