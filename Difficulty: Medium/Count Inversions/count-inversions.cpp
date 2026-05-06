53c641a6b409a004df1bd09dab02b40d0e23dc8c
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-05-06 08:08:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.cpp
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -2,23 +2,19 @@ class Solution {
   public:
   
     int merge(int low,int mid,int high,vector<int>&arr){
-        vector<int>temp;
         int i=low,j=mid+1;
+        vector<int>temp;
         int cnt=0;
         
         while(i<=mid && j<=high){
-            if(arr[i]<=arr[j]){
-                temp.emplace_back(arr[i++]);
-            }else{
+            if(arr[i]<=arr[j]) temp.emplace_back(arr[i++]);
+            else{
                 cnt+=mid-i+1;
                 temp.emplace_back(arr[j++]);
             }
         }
         
-        while(i<=mid){
-            temp.emplace_back(arr[i++]);
-        }
-        
+        while(i<=mid) temp.emplace_back(arr[i++]);
         while(j<=high){
             cnt+=mid-i+1;
             temp.emplace_back(arr[j++]);
@@ -31,14 +27,15 @@ class Solution {
     }
   
     int divide(int low,int high,vector<int>&arr){
-        if(low>=high) return 0;
         int cnt=0;
+        if(low>=high) return 0;
         int mid=(low+high)/2;
         cnt+=divide(low,mid,arr);
         cnt+=divide(mid+1,high,arr);
         cnt+=merge(low,mid,high,arr);
         return cnt;
     }
+  
     int inversionCount(vector<int> &arr) {
         // Code Here
         return divide(0,arr.size()-1,arr);
