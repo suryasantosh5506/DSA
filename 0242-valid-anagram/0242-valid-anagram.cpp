bda74fa362f8941c49aa39824e7ef1c8cefe46aa
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-05-08 09:33:09 +0530

    Time: 0 ms (100%), Space: 9.6 MB (91.89%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>arr(26,0);
+        if(s.size()!=t.size()) return false;
+        for(int i=0;i<s.size();i++){
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
+
+        for(auto x:arr){
+            if(x!=0) return false;
+        }
+        return true;
+    }
+};
