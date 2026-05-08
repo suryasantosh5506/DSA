7433b25893136f98cafc93e06e68b2031deac583
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-05-08 09:00:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Words/reverse-words.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    string reverseWords(string &s) {
+        // code here
+        string ans="";
+        string word="";
+
+        for(int i=s.size()-1;i>=0;i--){
+            if(!isalnum(s[i])){
+                if(!word.empty()){
+                    reverse(word.begin(),word.end());
+                    ans+=word+".";
+                    word="";
+                }
+            }else{
+                word+=s[i];
+            }
+        }
+
+        if(!word.empty()){
+            reverse(word.begin(),word.end());
+            ans+=word;
+            word="";          
+        }else{
+            ans.pop_back();
+        }
+        return ans;
+    }
+};
