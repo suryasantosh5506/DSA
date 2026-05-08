c7c3683babef24bfd7e71374a86a0d696d41d4e9
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-05-08 09:15:11 +0530

    Time: 0 ms (100%), Space: 9.9 MB (52.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,13 +3,11 @@ public:
     bool isPalindrome(string s) {
         int i=0,j=s.size()-1;
         while(i<j){
-            if(!isalnum(s[i])) i++;
-            else if(!isalnum(s[j])) j--;
-            else if(tolower(s[i])!=tolower(s[j])) return false;
-            else{
-                i++;
-                j--;
-            }
+            while(i<j && !isalnum(s[i])) i++;
+            while(i<j && !isalnum(s[j])) j--;
+            if(tolower(s[i])!=tolower(s[j])) return false;
+            i++;
+            j--;
         }
         return true;
     }
