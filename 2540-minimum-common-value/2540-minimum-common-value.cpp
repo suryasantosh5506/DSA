08d042b7f451b4dff601354c5b0f4a783a4159c5
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-05-19 08:01:16 +0530

    Time: 0 ms (100%), Space: 54.6 MB (30.11%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int common=INT_MAX;
         int n=nums1.size(),m=nums2.size();
         int i=0,j=0;
         while(i<n && j<m){
             if(nums1[i]==nums2[j]){
-                common=min(common,nums1[i]);
-                i++;
-                j++;
+                return nums1[i];
             }else if(nums1[i]>nums2[j]) j++;
             else i++;
         }
-        return (common==INT_MAX)?-1:common;
+        return -1;
     }
 };
