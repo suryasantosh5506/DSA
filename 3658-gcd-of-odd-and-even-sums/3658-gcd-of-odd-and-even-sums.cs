391f3a42b44948b63a6e16f51270b4bea4f479f1
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-07-15 07:28:34 +0530

    Time: 10 ms (24.24%), Space: 29.2 MB (39.39%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cs
@@ -0,0 +1,34 @@
+public class Solution {
+
+    int gcd(int a,int b){
+        while(b!=0){
+            int temp=a;
+            a=b;
+            b=temp%b;
+        }
+        return a;
+    }
+
+    public int GcdOfOddEvenSums(int n) {
+        int oddSum=0,evenSum=0;
+        int oddCount=n,evenCount=n;
+        int i=1;
+        while(oddCount>0){
+            if(i%2==1){
+                oddCount--;
+                oddSum+=i;
+            }
+            i++;
+        }
+
+        while(evenCount>0){
+            if(i%2==0){
+                evenSum+=i;
+                evenCount--;
+            }
+            i++;
+        }
+
+        return gcd(Math.Max(oddSum,evenSum),Math.Min(oddSum,evenSum));
+    }
+}
