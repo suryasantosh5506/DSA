fce0fb224ce2ca122dd1513ef9780aa2a9635a2e
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-07-31 08:51:07 +0530

    Time: 2 ms (37.33%), Space: 10 MB (60.35%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-
-    vector<vector<int>>ans;
-    vector<int>seq;
-
-    void solution(int i,vector<int>&nums){
-        if(i==nums.size()){
-            ans.emplace_back(seq);
-            return;
-        }
-
-        seq.emplace_back(nums[i]);
-        solution(i+1,nums);
-        seq.pop_back();
-        solution(i+1,nums);
-    }
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        solution(0,nums);
+        int n=nums.size();
+        int total=pow(2,n);
+        vector<vector<int>>ans;
+        for(int i=0;i<total;i++){
+            vector<int>seq;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j)) seq.emplace_back(nums[j]);
+            }
+            ans.emplace_back(seq);
+        }
         return ans;
     }
 };
