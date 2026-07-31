1ae24953fe81f09c736d239b840e3e937d91f3e6
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-07-31 09:03:54 +0530

    Time: 189 ms (47.59%), Space: 132.7 MB (8.87%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if(n<=2) return 0;
         vector<int>sieve(n);
-        sieve[0]=-1;
-        sieve[1]=-1;
-
-        for(int i=2;i<n;i++) sieve[i]=i;
+        for(int i=0;i<n;i++){
+            sieve[i]=i;
+        }
 
         for(int i=2;i*i<n;i++){
             if(sieve[i]==i){
-                for(int j=i+i;j<n;j+=i) sieve[j]=i;
+                for(int j=i+i;j<n;j+=i){
+                    sieve[j]=i;
+                }
             }
         }
 
-        int cnt=0;
+        int ans=0;
         for(int i=2;i<n;i++){
-            if(sieve[i]==i) cnt++;
+            if(sieve[i]==i) ans++;
         }
-        return cnt;
+        return ans;
     }
 };
