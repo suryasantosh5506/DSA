c8efe25f97a57a2dd1017753e9f1197b21730658
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-08-01 10:18:07 +0530

    Time: 81 ms (31.09%), Space: 193.4 MB (49.34%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -2,36 +2,34 @@ class Solution {
 public:
 
     const int mod=1e9+7;
+    vector<int>psee;
+    int n;
+    vector<int>nse;
 
-    vector<int> prevsmallestequalele(vector<int>&arr){
-        int n=arr.size();
+    void prevSmallEqual(vector<int>&arr){
         stack<int>st;
-        vector<int>ans(n);
         for(int i=0;i<n;i++){
-            while(!st.empty()&&arr[st.top()]>arr[i]) st.pop();
-            ans[i]=(st.empty())?-1:st.top();
+            while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
+            psee[i]=(st.empty())?-1:st.top();
             st.push(i);
         }
-        return ans;
     }
 
-    
-    vector<int> nextsmallestele(vector<int>&arr){
-        int n=arr.size();
+    void nextSmall(vector<int>&arr){
         stack<int>st;
-        vector<int>ans(n);
         for(int i=n-1;i>=0;i--){
-            while(!st.empty()&&arr[st.top()]>=arr[i]) st.pop();
-            ans[i]=(st.empty())?n:st.top();
+            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
+            nse[i]=(st.empty())?n:st.top();
             st.push(i);
         }
-        return ans;
     }
 
     int sumSubarrayMins(vector<int>& arr) {
-        vector<int>psee=prevsmallestequalele(arr);
-        vector<int>nse=nextsmallestele(arr);
-        int n=arr.size();
+        n=arr.size();
+        psee.resize(n);
+        nse.resize(n);
+        prevSmallEqual(arr);
+        nextSmall(arr);
         int ans=0;
         for(int i=0;i<n;i++){
             ans = (ans + ((i - psee[i]) * 1LL * (nse[i] - i) % mod * arr[i] % mod)) % mod;
