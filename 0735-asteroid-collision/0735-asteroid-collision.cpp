503159e224b2f5646e65d544cfd364a9f11bacc6
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-08-01 10:24:39 +0530

    Time: 0 ms (100%), Space: 21.4 MB (99.92%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int>st;
-        for(auto x:asteroids){
+        for(int &x:asteroids){
             if(x>0) st.emplace_back(x);
             else{
-                while(!st.empty() && st.back()>0 && abs(x)>st.back()) st.pop_back();
-                if(!st.empty() && abs(x)==st.back()) st.pop_back();
+                while(!st.empty() && st.back()>0 && st.back()<abs(x)) st.pop_back();
+                if(!st.empty() && st.back()==abs(x)) st.pop_back();
                 else if(st.empty() || st.back()<0) st.emplace_back(x);
             }
         }
