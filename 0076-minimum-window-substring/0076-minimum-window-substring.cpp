12d8ee9e3e6987b9383fa39548514e41089f404d
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-08-02 10:06:12 +0530

    Time: 3 ms (78.1%), Space: 11.5 MB (65.22%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         int n=s.size();
+        int m=t.size();
         unordered_map<char,int>mpp;
-        for(auto x:t) mpp[x]++;
-        int si=-1;
+        for(char &x:t) mpp[x]++;
         int i=0,j=0;
-        int mini=INT_MAX;
         int cnt=0;
-
+        int si=-1,len=INT_MAX;
         while(j<n){
             if(mpp[s[j]]>0) cnt++;
             mpp[s[j]]--;
-
-            while(cnt==t.size()){
-                if((j-i+1)<mini){
-                    mini=(j-i+1);
+            while(cnt==m){
+                if((j-i+1)<len){
+                    len=j-i+1;
                     si=i;
                 }
                 mpp[s[i]]++;
@@ -24,6 +22,6 @@ public:
             }
             j++;
         }
-        return (si==-1)?"":s.substr(si,mini);
+        return si==-1?"":s.substr(si,len);
     }
 };
