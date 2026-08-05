7ef59170818767df688c4a424113efc89ffc00db
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-08-05 09:02:59 +0530

    Time: 360 ms (33.1%), Space: 342.6 MB (34.83%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+    vector<vector<int>>graph;
+    vector<int>visited;
+    vector<int>checkvisited;
+
+    void dfs(int node){
+        visited[node]=1;
+
+        for(auto it:graph[node]){
+            if(!visited[it]) dfs(it);
+        }
+    }
+
+    bool canInvoke(int node){
+        checkvisited[node]=1;
+
+        for(auto it:graph[node]){
+            if(visited[it]) return true;
+            if(!checkvisited[it]){
+                if(canInvoke(it)) return true;
+            }
+        }
+        return false;
+    }
+
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        graph.resize(n);
+        visited.resize(n,0);
+        checkvisited.resize(n,0);
+        for(auto it:invocations){
+            graph[it[0]].emplace_back(it[1]);
+        }
+        dfs(k);
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(!visited[i]) ans.emplace_back(i);
+        }
+
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                if(canInvoke(i)){
+                    ans.clear();
+                    for(int i=0;i<n;i++) ans.emplace_back(i);
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
