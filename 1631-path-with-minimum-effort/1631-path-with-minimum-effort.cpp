480d94251c42cde0c0b82d139987936cd81845fa
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-08-09 09:12:58 +0530

    Time: 183 ms (11.52%), Space: 46.4 MB (13.45%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
 
-    bool isValid(int i,int j,int n,int m){
+    int n,m;
+
+    vector<int>dx={0,1,0,-1};
+    vector<int>dy={-1,0,1,0};
+
+    bool isValid(int i,int j){
         return (i>=0 && i<n) && (j>=0 && j<m);
     }
 
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int n=heights.size();
-        int m=heights[0].size();
+        n=heights.size();
+        m=heights[0].size();
 
-        vector<int>dx={0,1,0,-1};
-        vector<int>dy={-1,0,1,0};
-
-        vector<vector<int>>effTracer(n,vector<int>(m,INT_MAX));
+        vector<vector<int>>effort(n,vector<int>(m,INT_MAX));
         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
         pq.push({0,0,0});
-        // {effort,r,c}
+        effort[0][0]=0;
 
         while(!pq.empty()){
             auto top=pq.top();
@@ -25,20 +27,18 @@ public:
             int row=top[1];
             int col=top[2];
 
-            if(row==n-1 && col==m-1)
-                return eff;
+            if(row==n-1 && col==m-1) return eff;
 
             for(int k=0;k<4;k++){
                 int nr=row+dx[k];
                 int nc=col+dy[k];
 
-                if(isValid(nr,nc,n,m)){
-                    int newEff=abs(heights[nr][nc]-heights[row][col]);
-                    newEff=max(eff,newEff);
-
-                    if(newEff<effTracer[nr][nc]){
-                        pq.push({newEff,nr,nc});
-                        effTracer[nr][nc]=newEff;
+                if(isValid(nr,nc)){
+                    int neweffort=abs(heights[nr][nc]-heights[row][col]);
+                    neweffort=max(neweffort,effort[row][col]);
+                    if(neweffort<effort[nr][nc]){
+                        effort[nr][nc]=neweffort;
+                        pq.push({effort[nr][nc],nr,nc});
                     }
                 }
             }
