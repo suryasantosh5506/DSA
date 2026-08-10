e07ee7b33ce365c5c2be72b47029ee770bd4db24
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-08-10 07:46:50 +0530

    Time: 12 ms (38.4%), Space: 37.4 MB (31.86%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -2,18 +2,17 @@ typedef long long ll;
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        const int mod=1e9+7;
-        vector<vector<pair<ll,ll>>>graph(n);
-        vector<ll>distanceTracer(n,LLONG_MAX);
-        vector<ll>ways(n,0);
-        priority_queue<vector<ll>,vector<vector<ll>>,greater<vector<ll>>>pq;
-
+        vector<vector<pair<int,int>>>graph(n);
         for(auto it:roads){
             graph[it[0]].emplace_back(it[1],it[2]);
             graph[it[1]].emplace_back(it[0],it[2]);
         }
-
-        distanceTracer[0]=0;
+        const int mod=1e9+7;
+        vector<ll>dist(n,LLONG_MAX);
+        vector<ll>ways(n,0);
+        priority_queue<pair<ll,int>,vector<pair<ll,int>>,greater<pair<ll,int>>>pq;
+        // {dist,node}
+        dist[0]=0;
         ways[0]=1;
         pq.push({0,0});
 
@@ -21,21 +20,19 @@ public:
             auto top=pq.top();
             pq.pop();
 
-            ll dist=top[0];
-            ll node=top[1];
-
-            if(dist>distanceTracer[node]) continue;
+            ll d=top.first;
+            int node=top.second;
 
             for(auto it:graph[node]){
-                if(distanceTracer[it.first]>dist+it.second){
-                    distanceTracer[it.first]=dist+it.second;
-                    pq.push({dist+it.second,it.first});
+                if(dist[it.first]>d+it.second){
+                    dist[it.first]=d+it.second;
                     ways[it.first]=ways[node];
-                }else if(distanceTracer[it.first]==dist+it.second){
+                    pq.push({dist[it.first],it.first});
+                }else if(dist[it.first]==d+it.second){
                     ways[it.first]=(ways[it.first]+ways[node])%mod;
                 }
             }
         }
-        return ways[n-1]%mod;
+        return ways[n-1];
     }
 };
