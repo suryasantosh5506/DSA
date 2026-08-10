94cff26c2e3b6f8d99b675db52cb42a8ff6088d9
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-08-10 08:06:32 +0530

    Time: 19 ms (34.51%), Space: 17 MB (58.98%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        
         vector<vector<int>>dist(n,vector<int>(n,1e8));
 
         for(auto it:edges){
             dist[it[0]][it[1]]=it[2];
             dist[it[1]][it[0]]=it[2];
         }
-
+        
         for(int i=0;i<n;i++){
             dist[i][i]=0;
         }
 
+        int l=1e8;
         for(int k=0;k<n;k++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
-                    if(dist[i][k]!=1e8 && dist[k][j]!=1e8 && dist[i][j]>dist[i][k]+dist[k][j]){
+                    if(dist[i][k]!=l && dist[k][j]!=l && dist[i][j]>dist[i][k]+dist[k][j]){
                         dist[i][j]=dist[i][k]+dist[k][j];
                     }
                 }
             }
         }
 
-        int city=-1;
-        int reachable=INT_MAX;
+        int mini=INT_MAX;
+        int ans=-1;
 
         for(int i=0;i<n;i++){
             int cnt=0;
             for(int j=0;j<n;j++){
-                if(dist[i][j]<=distanceThreshold) cnt++;
+                if(i!=j && dist[i][j]<=distanceThreshold) cnt++;
             }
-            if(cnt<=reachable){
-                reachable=cnt;
-                city=i;
+
+            if(cnt<=mini){
+                mini=cnt;
+                ans=i;
             }
         }
-        return city;
+        return ans;
     }
 };
