2e053a898b5b56a547d5ad5db5ee860b13a8e2ff
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-09-12 13:27:08 +0530

    Time: 4 ms (91.15%), Space: 63.6 MB (74.67%) - LeetHub

---
+++ 0384-shuffle-an-array/0384-shuffle-an-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int>nums;
+    Solution(vector<int>& nums) {
+        this->nums=nums;
+    }
+    
+    vector<int> reset() {
+        return nums;
+    }
+    
+    vector<int> shuffle() {
+        vector<int>ans=nums;
+        int n=nums.size();
+        for(int i=n-1;i>=0;i--){
+            int j=rand()%(i+1);
+            swap(ans[i],ans[j]);
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
