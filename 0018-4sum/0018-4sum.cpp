22fb296ba5079fbf807c7fe87d229991fa7c8ef7
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-09-14 07:05:25 +0530

    Time: 19 ms (80.99%), Space: 17.4 MB (58.08%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,23 +1,20 @@
+typedef long long ll;
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>>ans;
         int n=nums.size();
         sort(nums.begin(),nums.end());
-
+        
         for(int i=0;i<n;i++){
             if(i!=0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n;j++){
                 if(j!=i+1 && nums[j]==nums[j-1]) continue;
-
                 int k=j+1,l=n-1;
-
                 while(k<l){
-                    long long sum=nums[i];
-                    sum+=nums[j];
+                    ll sum=nums[i]+nums[j];
                     sum+=nums[k];
                     sum+=nums[l];
-
                     if(sum==target){
                         ans.push_back({nums[i],nums[j],nums[k],nums[l]});
                         k++;
@@ -29,6 +26,7 @@ public:
                 }
             }
         }
+
         return ans;
     }
 };
