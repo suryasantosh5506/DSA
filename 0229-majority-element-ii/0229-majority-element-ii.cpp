ea24d8c850c7c31590de43d694cab325cc90d8cd
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-09-14 06:54:54 +0530

    Time: 0 ms (100%), Space: 23.7 MB (71.4%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int ele1=INT_MIN,ele2=INT_MIN;
+        int ele1=INT_MAX,ele2=INT_MAX;
         int cnt1=0,cnt2=0;
         int n=nums.size();
 
-        for(auto x:nums){
+        for(int &x:nums){
             if(cnt1==0 && x!=ele2){
                 ele1=x;
-                cnt1++;
+                cnt1=1;
             }else if(cnt2==0 && x!=ele1){
                 ele2=x;
-                cnt2++;
-            }else if(ele1==x) cnt1++;
-            else if(ele2==x) cnt2++;
+                cnt2=1;
+            }else if(x==ele1) cnt1++;
+            else if(x==ele2) cnt2++;
             else{
                 cnt1--;
                 cnt2--;
             }
         }
+
         cnt1=0;
         cnt2=0;
-        for(auto x:nums){
+
+        for(int &x:nums){
             if(x==ele1) cnt1++;
             else if(x==ele2) cnt2++;
         }
+
         vector<int>ans;
-        if(cnt1>n/3) ans.emplace_back(ele1);
-        if(cnt2>n/3) ans.emplace_back(ele2);
+        if(cnt1>(n/3)) ans.emplace_back(ele1);
+        if(cnt2>(n/3)) ans.emplace_back(ele2);
         return ans;
     }
 };
