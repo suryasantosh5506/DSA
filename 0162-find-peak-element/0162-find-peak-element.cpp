6e9a11b6b2db681766d19ebeaa9da4b763ec7d46
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-09-15 07:09:32 +0530

    Time: 0 ms (100%), Space: 12.7 MB (52.25%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -6,12 +6,11 @@ public:
         if(nums[0]>nums[1]) return 0;
         if(nums[n-1]>nums[n-2]) return n-1;
         int low=1,high=n-2;
-
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]) return mid;
-            if(nums[mid-1]<=nums[mid]) low=mid+1;
-            else high=mid-1;
+            if(nums[mid]<nums[mid-1]) high=mid-1;
+            else low=mid+1;
         }
         return -1;
     }
