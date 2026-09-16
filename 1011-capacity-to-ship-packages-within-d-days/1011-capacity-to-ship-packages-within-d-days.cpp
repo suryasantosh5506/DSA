219ecc7f9a7278307c94b50f4822b8dc3b08f79a
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-09-16 06:39:30 +0530

    Time: 13 ms (40.73%), Space: 35.1 MB (19.93%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
 
-    bool possible(int mid,vector<int>& weights, int days){
-        int cap=0;
-        int totaldays=1;
-
-        for(auto x:weights){
+    bool possible(int mid,vector<int>& arr, int days){
+        int d=1,cap=0;
+        for(int &x:arr){
             if(cap+x>mid){
-                totaldays++;
                 cap=0;
+                d++;
             }
             cap+=x;
-            if(totaldays>days) return false;
+            if(d>days) return false;
         }
         return true;
     }
 
-    int shipWithinDays(vector<int>& weights, int days) {
-        int low=*max_element(weights.begin(),weights.end());
-        int high=accumulate(weights.begin(),weights.end(),0);
-
+    int shipWithinDays(vector<int>& arr, int days) {
+        int n=arr.size();
+        int low=*max_element(arr.begin(),arr.end());
+        int high=accumulate(arr.begin(),arr.end(),0);
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(possible(mid,weights,days)) high=mid-1;
+            if(possible(mid,arr,days))high=mid-1;
             else low=mid+1;
         }
         return low;
