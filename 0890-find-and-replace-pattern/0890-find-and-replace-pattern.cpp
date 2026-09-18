666a30661b191635d62667d2e239ccc7386e34bb
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-09-18 06:40:18 +0530

    Time: 0 ms (100%), Space: 11.8 MB (26.36%) - LeetHub

---
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    bool isIsomorphic(string s, string t) {
+        int n=s.size(),m=t.size();
+        if(n!=m) return false;
+        unordered_map<char,char>stot,ttos;
+        for(int i=0;i<n;i++){
+            if(stot.count(s[i])) if(stot[s[i]]!=t[i]) return false;
+            if(ttos.count(t[i])) if(ttos[t[i]]!=s[i]) return false;
+            stot[s[i]]=t[i];
+            ttos[t[i]]=s[i];
+        }
+        return true;
+    }
+
+
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string>ans;
+        for(string &word:words){
+            if(isIsomorphic(word,pattern)) ans.emplace_back(word);
+        }
+        return ans;
+    }
+};
