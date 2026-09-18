2f255bc6b810069ada422d1a27fa8f39f49bc943
surya_santosh <154221969+suryasantosh5506@users.noreply.github.com>
2026-09-18 06:37:43 +0530

    Time: 4 ms (22.53%), Space: 9.2 MB (82.71%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char>atob,btoa;
-        int n=s.size();
-        int m=t.size();
+        int n=s.size(),m=t.size();
         if(n!=m) return false;
+        unordered_map<char,char>stot,ttos;
         for(int i=0;i<n;i++){
-            if(atob.find(s[i])==atob.end()) atob[s[i]]=t[i];
-            else if(atob[s[i]]!=t[i]) return false;
-
-            if(btoa.find(t[i])==btoa.end()) btoa[t[i]]=s[i];
-            else if(btoa[t[i]]!=s[i]) return false;
+            if(stot.count(s[i])) if(stot[s[i]]!=t[i]) return false;
+            if(ttos.count(t[i])) if(ttos[t[i]]!=s[i]) return false;
+            stot[s[i]]=t[i];
+            ttos[t[i]]=s[i];
         }
         return true;
     }
